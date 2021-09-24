9a21d5a5cfb2589d59fd549ec6c43a25052a2a93
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-09-24 09:15:46 +0100

    Adding counter.jsx

---
+++ src/components/counter.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: 2,
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increase</button>
+      </React.Fragment>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
